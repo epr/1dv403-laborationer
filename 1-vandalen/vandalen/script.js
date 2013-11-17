1a3e1f240a70b467709399a3fe15b73f7e15ec59
Eddy Proca <eddy@Eddy-Procas-MacBook-Pro.local>
2013-11-18 00:36:32 +0100

    wrote vandalen code

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,37 @@
 "use strict";
-
 var makePerson = function (persArr) {
-
-
-	// Din kod här...
-
+    var ages = [], namesArr = [];
+    persArr.map(function (pers) {
+        ages.push(pers.age);
+        namesArr.push(pers.name);
+    });
+    return {
+        minAge : ages.sort()[0],
+        maxAge : ages.sort().reverse()[0],
+        averageAge : Math.round(ages.reduce(function (a, b) {
+            return a + b;
+        }) / ages.length),
+        names : namesArr.sort(function (a, b) {
+            return a.localeCompare(b);
+        }).join(", ")
+    };
 };
 
+/*var makePerson = function (persArr) {
+    var ages = [];
+    var ageSum = 0;
+    var namesArr = [];
+    for (var i = 0; i < persArr.length; i++) {
+        ages[i] = persArr[i].age;
+        ageSum = ageSum + persArr[i].age;
+        namesArr[i] = persArr[i].name;
+    }
+    return {
+        minAge : ages.sort()[0],
+        maxAge : ages.sort().reverse()[0],
+        averageAge : Math.round(ageSum / ages.length),
+        names : namesArr.sort(function (a, b) {
+            return a.localeCompare(b);
+        }).join(", ")
+    };
+};*/
