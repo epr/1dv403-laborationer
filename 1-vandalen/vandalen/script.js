23e4cdebca9e0543ae35c3d54ff0e62cad2c42bc
Eddy Proca <eddy@Eddy-Procas-MacBook-Pro.local>
2013-11-26 16:18:08 +0100

    began extra assignment

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,5 +1,27 @@
 "use strict";
 var makePerson = function (persArr) {
+    if (persArr.length > 0) {
+        var ages = [], namesArr = [];
+        persArr.map(function (pers) {
+            ages.push(pers.age);
+            namesArr.push(pers.name);
+        });
+        return {
+            minAge : ages.sort()[0],
+            maxAge : ages.sort().reverse()[0],
+            averageAge : Math.round(ages.reduce(function (a, b) {
+                return a + b;
+            }) / ages.length),
+            names : namesArr.sort(function (a, b) {
+                return a.localeCompare(b);
+            }).join(", ")
+        };
+    } else {
+        throw new Error;
+    }
+};
+
+/*var makePerson = function (persArr) {
     var ages = [], namesArr = [];
     persArr.map(function (pers) {
         ages.push(pers.age);
@@ -15,7 +37,7 @@ var makePerson = function (persArr) {
             return a.localeCompare(b);
         }).join(", ")
     };
-};
+};*/
 
 /*var makePerson = function (persArr) {
     var ages = [];
