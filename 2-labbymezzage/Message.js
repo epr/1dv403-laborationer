9184bea2ca5ab186e7aa5e8a6a65622e558a2217
Eddy Proca <eddy@Eddy-Procas-MacBook-Pro.local>
2013-12-05 21:18:26 +0100

    finished labby mezzage

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -17,8 +17,47 @@ Message.prototype.toString = function () {
     return this.getText() + " (" + this.getDate() + ")";
 };
 Message.prototype.getHTMLText = function () {
-    
+    return this.getText().replace(/[\n\r]/g, "<br>");
 };
 Message.prototype.getDateText = function () {
-    
+    var month;
+    switch (this.getDate().getMonth()) {
+    case 0:
+        month = "januari";
+        break;
+    case 1:
+        month = "februari";
+        break;
+    case 2:
+        month = "mars";
+        break;
+    case 3:
+        month = "april";
+        break;
+    case 4:
+        month = "maj";
+        break;
+    case 5:
+        month = "juni";
+        break;
+    case 6:
+        month = "juli";
+        break;
+    case 7:
+        month = "augusti";
+        break;
+    case 8:
+        month = "september";
+        break;
+    case 9:
+        month = "oktober";
+        break;
+    case 10:
+        month = "november";
+        break;
+    case 11:
+        month = "december";
+        break;
+    }
+    return "Inlägget skapades den " + this.getDate().getDate() + " " + month + " " + this.getDate().getFullYear() + " klockan " + this.getDate().getHours() + ":" + this.getDate().getMinutes() + ":" + this.getDate().getSeconds();
 };
--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,21 +1,65 @@
+"use strict";
 var MessageBoard = {
     messages : [],
-    init : function(e){
-        var mess = new Message("test", new Date());
-        MessageBoard.messages.push(mess);
-        MessageBoard.renderMessages();
+    init : function (e) {
+        var textArea = document.getElementById("input");
+        document.getElementById("send").addEventListener("click", function () {
+            MessageBoard.messages.push(new Message(textArea.value, new Date()));
+            MessageBoard.renderMessages();
+        }, false);
+        textArea.addEventListener("keydown", function (e) {
+            if ((e.keyCode === 13) && !e.shiftKey) {
+                e.preventDefault();
+                MessageBoard.messages.push(new Message(textArea.value, new Date()));
+                MessageBoard.renderMessages();
+            }
+        }, false);
+        MessageBoard.updateCounter();
     },
-    renderMessages : function() {
+    renderMessages : function () {
+        var i;
+        document.getElementById("input").value = "";
         document.getElementById("messages").innerHTML = "";
-        for (var i = 0; i < MessageBoard.messages.length; i++) {
+        for (i = 0; i < MessageBoard.messages.length; i++) {
             MessageBoard.renderMessage(i);
         }
+        MessageBoard.updateCounter();
     },
-    renderMessage : function(messageId) {
-        var article = document.createElement("article");
-        var p = document.createElement("p");
-        var text = document.createTextNode(MessageBoard.messages[messageId].getText());
-        document.getElementById("messages").appendChild(article).appendChild(p).appendChild(text);
+    renderMessage : function (messageId) {
+        var article = document.createElement("article"),
+            p = document.createElement("p"),
+            close = document.createElement("button"),
+            clock = document.createElement("button"),
+            timeStamp = document.createElement("time"),
+            time = document.createTextNode(MessageBoard.messages[messageId].getDate().getHours() + ":" + MessageBoard.messages[messageId].getDate().getMinutes() + ":" + MessageBoard.messages[messageId].getDate().getSeconds()),
+            dateTime = MessageBoard.messages[messageId].getDate();
+        close.className = "close";
+        close.setAttribute("title", "close");
+        close.addEventListener("click", function () {
+            if (window.confirm("Vill du verkligen radera meddelandet?")) {
+                MessageBoard.removeMessage(messageId);
+            }
+        }, false);
+        clock.className = "clock";
+        clock.setAttribute("title", "show time");
+        clock.addEventListener("click", function () {
+            alert(MessageBoard.messages[messageId].getDateText());
+        }, false);
+        article.appendChild(close);
+        article.appendChild(clock);
+        p.innerHTML = MessageBoard.messages[messageId].getHTMLText();
+        document.getElementById("messages").appendChild(article).appendChild(p);
+        timeStamp.setAttribute("datetime", dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds());
+        article.appendChild(timeStamp).appendChild(time);
+    },
+    removeMessage : function (messageId) {
+        MessageBoard.messages.splice(messageId, 1);
+        MessageBoard.renderMessages();
+    },
+    updateCounter : function () {
+        var counter = document.createElement("p"),
+            countertext = document.createTextNode("Antal meddelanden: " + MessageBoard.messages.length);
+        document.getElementById("messages").appendChild(counter).appendChild(countertext);
     }
 };
 
