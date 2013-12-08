43b624598b466c7f4ca25b70c7248f3f51616574
Eddy Proca <eddy@Eddy-Procas-MacBook-Pro.local>
2013-12-08 13:20:30 +0100

    nothing

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -22,42 +22,42 @@ Message.prototype.getHTMLText = function () {
 Message.prototype.getDateText = function () {
     var month;
     switch (this.getDate().getMonth()) {
-    case 0:
-        month = "januari";
-        break;
-    case 1:
-        month = "februari";
-        break;
-    case 2:
-        month = "mars";
-        break;
-    case 3:
-        month = "april";
-        break;
-    case 4:
-        month = "maj";
-        break;
-    case 5:
-        month = "juni";
-        break;
-    case 6:
-        month = "juli";
-        break;
-    case 7:
-        month = "augusti";
-        break;
-    case 8:
-        month = "september";
-        break;
-    case 9:
-        month = "oktober";
-        break;
-    case 10:
-        month = "november";
-        break;
-    case 11:
-        month = "december";
-        break;
+        case 0:
+            month = "januari";
+            break;
+        case 1:
+            month = "februari";
+            break;
+        case 2:
+            month = "mars";
+            break;
+        case 3:
+            month = "april";
+            break;
+        case 4:
+            month = "maj";
+            break;
+        case 5:
+            month = "juni";
+            break;
+        case 6:
+            month = "juli";
+            break;
+        case 7:
+            month = "augusti";
+            break;
+        case 8:
+            month = "september";
+            break;
+        case 9:
+            month = "oktober";
+            break;
+        case 10:
+            month = "november";
+            break;
+        case 11:
+            month = "december";
+            break;
     }
     return "Inlägget skapades den " + this.getDate().getDate() + " " + month + " " + this.getDate().getFullYear() + " klockan " + this.getDate().getHours() + ":" + this.getDate().getMinutes() + ":" + this.getDate().getSeconds();
 };
