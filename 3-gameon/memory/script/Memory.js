c41757f99462cb1c4a8749f20e76dfe889183b19
Eddy Proca <eddy@Eddy-Procas-MacBook-Pro.local>
2013-12-11 20:37:35 +0100

    added score board

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -1,8 +1,28 @@
+/*jslint browser:true*/
 var Memory = {
+    rows : 4,
+    cols : 4,
+    one : 0,
+    two : 0,
+    turn : 1,
     init : function () {
         "use strict";
-        var table = Memory.createTable(4, 4);
-        Memory.displayGame(table);
+        var table,
+            button = document.getElementById("new"),
+            scoreBoard = document.getElementById("score");
+        if ((Memory.rows * Memory.cols < 17) && (Memory.rows * Memory.cols % 2 === 0)) {
+            table = Memory.createTable(Memory.rows, Memory.cols);
+            Memory.displayGame(table);
+        } else {
+            throw new Error("Change rows or cols.");
+        }
+        button.addEventListener("click", function () {
+            Memory.init();
+        }, false);
+        Memory.one = 0;
+        Memory.two = 0;
+        Memory.turn = 1;
+        Memory.updateScore();
     },
     createTable : function (rows, cols) {
         "use strict";
@@ -26,6 +46,7 @@ var Memory = {
             a,
             i,
             j;
+        div.innerHTML = "";
         for (i = 0; i < positions.length; i += 1) {
             ol = document.createElement("ol");
             for (j = 0; j < positions[i].length; j += 1) {
@@ -35,6 +56,7 @@ var Memory = {
             }
             div.appendChild(ol);
         }
+        Memory.updateScore();
     },
     createCard : function (id) {
         "use strict";
@@ -59,7 +81,8 @@ var Memory = {
     flipCard : function (img, src) {
         "use strict";
         var match,
-            timer;
+            timer,
+            aside = document.getElementById("score");
         if (img.getAttribute("src") !== src) {
             if (Memory.flippedCard === null) {
                 Memory.flippedCard = img;
@@ -68,14 +91,40 @@ var Memory = {
                 match = Memory.compareCards(Memory.flippedCard.getAttribute("src"), src);
                 img.setAttribute("src", src);
                 if (!match) {
+                    if (Memory.turn === 1) {
+                        Memory.turn = 2;
+                        
+                    } else {
+                        Memory.turn = 1;
+                    }
                     Memory.timer = window.setTimeout(function () {
                         Memory.flippedCard.setAttribute("src", "pics/0.png");
                         img.setAttribute("src", "pics/0.png");
                         Memory.flippedCard = null;
                         Memory.timer = null;
-                    }, 1000);
+                        Memory.updateScore();
+                    }, 500);
                 } else {
+                    Memory.flippedCard.className = "found";
+                    img.className = "found";
+                    Memory.found += 2;
                     Memory.flippedCard = null;
+                    if (Memory.turn === 1) {
+                        Memory.one += 1;
+                    } else {
+                        Memory.two += 1;
+                    }
+                    Memory.updateScore();
+                    if (Memory.found === Memory.rows * Memory.cols) {
+                        if (Memory.one > Memory.two) {
+                            aside.getElementsByTagName("h2")[0].innerHTML = "Player 1 wins!";
+                        } else if (Memory.two > Memory.one) {
+                            aside.getElementsByTagName("h2")[1].innerHTML = "Player 2 wins!";
+                        } else {
+                            aside.innerHTML = "It's a tie.";
+                        }
+                        timer = window.setTimeout(Memory.init, 2000);
+                    }
                 }
             }
         }
@@ -89,6 +138,32 @@ var Memory = {
         }
     },
     flippedCard : null,
-    timer : null
+    timer : null,
+    found : 0,
+    updateScore : function () {
+        "use strict";
+        var aside = document.getElementById("score"),
+            h2,
+            p;
+        aside.innerHTML = "";
+        h2 = document.createElement("h2");
+        h2.innerHTML = "Player 1";
+        if (Memory.turn === 1) {
+            h2.className = "current";
+        }
+        p = document.createElement("p");
+        p.innerHTML = Memory.one;
+        aside.appendChild(h2);
+        aside.appendChild(p);
+        h2 = document.createElement("h2");
+        h2.innerHTML = "Player 2";
+        if (Memory.turn === 2) {
+            h2.className = "current";
+        }
+        p = document.createElement("p");
+        p.innerHTML = Memory.two;
+        aside.appendChild(h2);
+        aside.appendChild(p);
+    }
 };
 window.onload = Memory.init;
