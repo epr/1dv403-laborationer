d681291d0680f940a219676d3b5b8cc900129063
Eddy Proca <eddy@Eddy-Procas-MacBook-Pro.local>
2013-12-20 18:45:56 +0100

    made windows appear in different places

--- 5-pwd/Desktop.js
+++ 5-pwd/Desktop.js
@@ -1,4 +1,6 @@
 var Desktop = {
+    lastAppPosX : 10,
+    lastAppPosY : 10,
     init : function () {
         "use strict";
         var galleryIcon = document.getElementById("gallery-icon"),
@@ -10,21 +12,35 @@ var Desktop = {
             Desktop.openRssApp();
         }, false);
     },
-    openApp : function (title, appClass) {
+    openApp : function (title, appClass, appWidth, appHeight) {
         "use strict";
         var desktop = document.getElementById("desktop"),
+            iconBar = document.getElementById("icon-bar"),
             appWindow = document.createElement("div"),
             titleBar = document.createElement("header"),
             appTitle = document.createElement("h2"),
             titleText = document.createTextNode(title);
         appWindow.classList.add(appClass, "app");
+        if ((desktop.clientWidth - appWidth - 10) < Desktop.lastAppPosX) {
+            Desktop.lastAppPosX = 10;
+        }
+        if ((desktop.clientHeight - appHeight - 10 - iconBar.clientHeight) < Desktop.lastAppPosY) {
+            Desktop.lastAppPosY = 10;
+        }
+        appWindow.style.left = Desktop.lastAppPosX + "px";
+        appWindow.style.top = Desktop.lastAppPosY + "px";
+        appWindow.style.width = appWidth + "px";
+        appWindow.style.height = appHeight + "px";
+        Desktop.lastAppPosX += 10;
+        Desktop.lastAppPosY += 10;
         desktop.appendChild(appWindow).appendChild(titleBar).appendChild(appTitle).appendChild(titleText);
+        console.log(desktop.clientWidth);
     },
     openGalleryApp : function () {
-        Desktop.openApp("Image gallery", "gallery-app");
+        Desktop.openApp("Image gallery", "gallery-app", 300, 200);
     },
     openRssApp: function () {
-        Desktop.openApp("Rss feed", "rss-app");
+        Desktop.openApp("Rss feed", "rss-app", 400, 280);
     }
 };
 document.load = Desktop.init();
